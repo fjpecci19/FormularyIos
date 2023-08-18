4330c5c8f4a90139da61eddf2523118a00fa5042
Franco Pecci <fapeccisan@gmail.com>
2023-08-18 11:13:26 -0400

    Verificación de campos completados

--- Formulario/ViewController.swift
+++ Formulario/ViewController.swift
@@ -13,15 +13,24 @@ protocol ValidationDelegate: AnyObject {
     func isValidTin(_ tin: String) -> Bool
 }
 
-class ViewController: UIViewController, ValidationDelegate, UITextFieldDelegate {
+class ViewController: UIViewController, ValidationDelegate, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var date: UIDatePicker!
+    @IBOutlet weak var sex: UITextField!
+    @IBOutlet weak var civilstatus: UITextField!
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var phonenumber: UITextField!
     @IBOutlet weak var tin: UITextField!
     @IBOutlet weak var sign: UIButton!
     
+    let defaultDate = Date()
+    let genderOptions = ["Male", "Female"]
+    let maritalStatusOptions = ["Single", "Married", "Divorced", "Widowed"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        createPickerView(for: sex, with: genderOptions)
+        createPickerView(for: civilstatus, with: maritalStatusOptions)
         email.delegate = self
         phonenumber.delegate = self
         tin.delegate = self
@@ -42,11 +51,45 @@ class ViewController: UIViewController, ValidationDelegate, UITextFieldDelegate 
             showAlert(message: "The TIN must have 7 numbers, a hyphen and a number at the end")
             return
         }
-        
-        phonenumber.text = ""
-        email.text = ""
-        tin.text = ""
         showSucceed(message: "Successful registration")
+        resetFields()
+    }
+    
+    func createPickerView(for textField: UITextField, with options: [String]) {
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        textField.inputView = pickerView
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == sex.inputView as? UIPickerView {
+            return genderOptions.count
+        } else if pickerView == civilstatus.inputView as? UIPickerView {
+            return maritalStatusOptions.count
+        }
+        return 0
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == sex.inputView as? UIPickerView {
+            return genderOptions[row]
+        } else if pickerView == civilstatus.inputView as? UIPickerView {
+            return maritalStatusOptions[row]
+        }
+        return nil
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == sex.inputView as? UIPickerView {
+            sex.text = genderOptions[row]
+        } else if pickerView == civilstatus.inputView as? UIPickerView {
+            civilstatus.text = maritalStatusOptions[row]
+        }
     }
     
     func isValidEmail(_ email: String) -> Bool {
@@ -84,5 +127,14 @@ class ViewController: UIViewController, ValidationDelegate, UITextFieldDelegate 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+    func resetFields() {
+        date.setDate(defaultDate, animated: true)
+        sex.text = ""
+        civilstatus.text = ""
+        phonenumber.text = ""
+        email.text = ""
+        tin.text = ""
     }
 }
